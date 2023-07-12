01f1656a2aeb5d216b571201a4115d52e3815b51
aliiiimaher <alimohammadmaher@gmail.com>
2023-07-12 10:11:31 +0330

    card style added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,6 @@
-import teacherSvg from "../assets/teacher.svg"
+import "../styles/components/Card.scss";
+
+import teacherSvg from "../assets/teacher.svg";
 
 interface Props {
   icon?: string;
@@ -21,17 +23,19 @@ function Card({
       <div className="card" style={{ width: "18rem" }}>
         <img src={icon} className="card-img-top" alt="card image" />
         <div className="card-body">
-          <h5 className="card-title" dir="rtl">{courseTitle}</h5>
+          <h5 className="card-title" dir="rtl">
+            {courseTitle}
+          </h5>
           <p className="card-text">{description}</p>
           <div className="teacher-container">
-            <h3>{courseTeacher}</h3>
             <img src={teacherSvg} alt="teacherSvg" />
+            <h3>{courseTeacher}</h3>
           </div>
           <div>
+            {coursePrice}
             <a href="#" className="btn btn-primary">
               Go somewhere
             </a>
-            {coursePrice}
           </div>
         </div>
       </div>
