895bd8208b5e56e6c06ed59a7715ac80c4e7e924
aliiiimaher <alimohammadmaher@gmail.com>
2023-07-12 09:45:59 +0330

    card component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import InputBox from "./components/InputBox";
-import Button from "./components/Button"
+import Button from "./components/Button";
+import Card from "./components/Card";
 
-import personSvg from "./assets/person.svg"
+import personSvg from "./assets/person.svg";
 
 function App() {
   return (
     <div>
       <InputBox placeHolder="یوزنیم" icon={personSvg} />
       <Button text="hello" size="large" />
+      <Card
+        icon={personSvg}
+        courseTitle="english"
+        description="the best of all ..."
+        courseTeacher="ali"
+        coursePrice={20}
+      />
     </div>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,42 @@
+import teacherSvg from "../assets/teacher.svg"
+
+interface Props {
+  icon?: string;
+  courseTitle: string;
+  description?: string;
+  courseTeacher: string;
+  coursePrice: number;
+}
+
+function Card({
+  icon,
+  courseTitle,
+  description,
+  courseTeacher,
+  coursePrice,
+}: Props) {
+  return (
+    <>
+      {/* this is a card {courseTitle} and {courseTeacher} */}
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={icon} className="card-img-top" alt="card image" />
+        <div className="card-body">
+          <h5 className="card-title">{courseTitle}</h5>
+          <p className="card-text">{description}</p>
+          <div className="teacher-container">
+            <h3>{courseTeacher}</h3>
+            <img src={teacherSvg} alt="teacherSvg" />
+          </div>
+          <div>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+            {coursePrice}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
