99b33cd77adaa6504ca8ff053d37ab09160b4102
aliiiimaher <alimohammadmaher@gmail.com>
2023-07-12 16:52:35 +0330

    header complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,33 @@
-import Button from "./Button"
+import Button from "./Button";
+import "../styles/components/Header.scss";
 
 import logoPng from "../assets/Pic/Frame.png";
 
 function Header() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#3a365e" }}>
-        <div className="container-fluid">
+      <nav
+        className="navbar navbar-expand-lg"
+        style={{ backgroundColor: "#3a365e" }}
+      >
+        <div
+          className="container-fluid"
+          style={{ marginLeft: "40px", marginRight: "40px" }}
+        >
           <a className="navbar-brand" href="#">
             <img src={logoPng} alt="Logo" width="80" height="63" />
           </a>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div
+            className="collapse navbar-collapse menu-items"
+            style={{ justifyContent: "flex-end" }}
+            id="navbarSupportedContent"
+          >
+            <ul
+              className="navbar-nav mb-2 mb-lg-0 menu-items"
+              style={{ marginLeft: "auto" }}
+            >
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link" aria-current="page" href="#">
                   خانه
                 </a>
               </li>
@@ -27,13 +41,10 @@ function Header() {
                   درباره ما
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
             </ul>
           </div>
-          <Button text="ثبت نام" size="large"/>
-          <Button text="ورود" size="large"/>
+            <Button text="ثبت نام" size="large" />
+            <Button text="ورود" size="large" />
         </div>
       </nav>
     </>
