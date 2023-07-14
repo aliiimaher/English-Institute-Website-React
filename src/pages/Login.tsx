7cb3d8eec4e8ccd12c55f8d987a38e6ebb9fabdc
aliiiimaher <alimohammadmaher@gmail.com>
2023-07-14 11:06:39 +0330

    login-page style added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,5 @@
+import "../styles/pages/Login.scss";
+
 import InputBox from "../components/InputBox";
 import Button from "../components/Button";
 
@@ -8,52 +10,21 @@ import loginSvg from "../assets/Pic/loginSvg.svg";
 function Login() {
   return (
     <>
-      <div
-        className="login-container"
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "40px",
-          marginBottom: "40px",
-          backgroundColor: "#1C1A2D",
-        }}
-      >
-        <div
-          className="login-right-side"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingLeft: "120px",
-            backgroundColor: "#1C1A2D",
-            color: "#EAD2B7",
-            paddingRight: "20%",
-          }}
-        >
+      <div className="login-container">
+        <div className="login-right-side">
           <h1>از اینجا وارد شوید!</h1>
           <h3>برای دسترسی به خدمات میبایست ابتدا وارد حساب خود شوید.</h3>
           <InputBox placeHolder="نام کاربری یا ایمیل" icon={personSvg} />
           <InputBox placeHolder="رمز عبور" icon={passwordSvg} />
           <Button text="ورود" size="large" />
         </div>
-        <div
-          className="login-left-side"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "column",
-            alignItems: "center",
-            paddingRight: "120px",
-            // paddingLeft: "120px",
-            backgroundColor: "#3A365E",
-            paddingLeft: "20%",
-            color: "#EAD2B7",
-          }}
-        >
+        <div className="login-left-side">
           <h1>سلام دوباره!</h1>
-          <h3>وبسایت زبان لرن شامل آموزش‌های زیادی هست.</h3>
-          <h3>ما انواع آموزش‌های آفلاین، آنلاین و حضوری رو داریم.</h3>
+          <h3>
+            وبسایت زبان لرن شامل آموزش‌های زیادی هست.
+            <br />
+            ما انواع آموزش‌های آفلاین، آنلاین و حضوری رو داریم.
+          </h3>
           <img src={loginSvg} alt="login-svg" />
         </div>
       </div>
