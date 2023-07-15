354da652199b93b2107af9dfdd2f8ebf67411191
aliiiimaher <alimohammadmaher@gmail.com>
2023-07-15 03:36:47 +0330

    first side of register page added

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,23 @@
 function Register() {
-  return <>this is register page</>;
+  return (
+    <>
+      <div className="register-container">
+        <div className="register-right-side">
+          <div className="register-right-side-up"></div>
+          <div className="register-right-side-down"></div>
+        </div>
+        <div className="register-left-side">
+          <h1>سلام!</h1>
+          <h3>
+            به جمع زبان لرنی‌ها خیلی خوش اومدی!
+            <br />
+            اینجا قراره با هم مثل آب خوردن هر زبونی رو یاد بگیریم!
+          </h3>
+          <img src={} alt="register-svg"/>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Register;
