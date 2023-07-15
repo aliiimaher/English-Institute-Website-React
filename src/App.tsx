c127d56300dc0b82eb3820516776adc0a08854a0
aliiiimaher <alimohammadmaher@gmail.com>
2023-07-15 15:58:34 +0330

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import HomePage from "./pages/Home";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
 import TestPage from "./pages/Test";
@@ -14,14 +15,12 @@ function App() {
       <Header />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/test" element={<TestPage />} />
         </Routes>
       </BrowserRouter>
-      عرض ادب
-      <br />
-      طول ادب
       <Footer />
     </>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <>this is home</>;
+}
+
+export default Home;
