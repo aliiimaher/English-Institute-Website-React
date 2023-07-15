cc1f1df5afa9cb02611868852fffca3c3109c79d
aliiiimaher <alimohammadmaher@gmail.com>
2023-07-15 17:05:48 +0330

    course list in home-page added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,60 @@
 import Button from "../components/Button";
+import Card from "../components/Card";
+
+import "../styles/pages/Home.scss";
 
 import homePageSvg1 from "../assets/Pic/homePageSvg1.svg";
 import homePageSvg2 from "../assets/Pic/homePageSvg2.svg";
 import homePageSvg3 from "../assets/Pic/homePageSvg3.svg";
+import englishCoursePng from "../assets/Pic/learn-english-language.png";
 
 function Home() {
   return (
     <>
       <div className="home-page-ads-1">
-        <img src={homePageSvg1} alt="home page svg 1" />
-        <div>
+        <div style={{ maxWidth: "572px" }}>
           <h1>زبان را با ما بیاموزید!</h1>
-          <h3>
+          <h2 style={{ fontSize: "28px", marginBottom: "56px" }}>
             آکادمی زبان لرن با بیش از 5 سال سابقه حرفه‌ای در آموزش زبان‌های زنده
             دنیا شما را همراهی می‌کند.
-          </h3>
+          </h2>
           <Button text="مشاهده دوره‌های آموزشی" size="large" />
         </div>
+        <img src={homePageSvg1} alt="home page svg 1" />
+      </div>
+      <div className="home-page-courses-in-row">
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <h1>دوره‌های ویژه</h1>
+            <h3>سایر دوره‌ها --</h3>
+        </div>
+        <Card
+          icon={englishCoursePng}
+          courseTitle="آموزش مکالمه زبان انگلیسی"
+          description="در سرتاسر دنیا، چه شرق چه ..."
+          courseTeacher="مهدی وکیلی"
+          coursePrice=" ۲۰,۰۰۰ تومان"
+        />
+        <Card
+          icon={englishCoursePng}
+          courseTitle="آموزش مکالمه زبان انگلیسی"
+          description="در سرتاسر دنیا، چه شرق چه ..."
+          courseTeacher="مهدی وکیلی"
+          coursePrice=" ۲۰,۰۰۰ تومان"
+        />
+        <Card
+          icon={englishCoursePng}
+          courseTitle="آموزش مکالمه زبان انگلیسی"
+          description="در سرتاسر دنیا، چه شرق چه ..."
+          courseTeacher="مهدی وکیلی"
+          coursePrice=" ۲۰,۰۰۰ تومان"
+        />
+        <Card
+          icon={englishCoursePng}
+          courseTitle="آموزش مکالمه زبان انگلیسی"
+          description="در سرتاسر دنیا، چه شرق چه ..."
+          courseTeacher="مهدی وکیلی"
+          coursePrice=" ۲۰,۰۰۰ تومان"
+        />
       </div>
     </>
   );
