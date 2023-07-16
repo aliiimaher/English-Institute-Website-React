bae4784bf0e0fe4216fa2f5783696bac5d778f69
aliiiimaher <alimohammadmaher@gmail.com>
2023-07-16 15:18:44 +0330

    courses page added

---
+++ src/pages/Courses.tsx
@@ -0,0 +1,101 @@
+import Card from "../components/Card";
+
+import englishCoursePng from "../assets/Pic/learn-english-language.png";
+
+function Courses() {
+  return (
+    <>
+      <div className="courses-page-container">
+        <div className="courses-page-container-right">drop-down-lists</div>
+        <div className="courses-page-container-left">
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />{" "}
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />{" "}
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+          <Card
+            icon={englishCoursePng}
+            courseTitle="آموزش مکالمه زبان انگلیسی"
+            description="در سرتاسر دنیا، چه شرق چه ..."
+            courseTeacher="مهدی وکیلی"
+            coursePrice=" ۲۰,۰۰۰ تومان"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Courses;
