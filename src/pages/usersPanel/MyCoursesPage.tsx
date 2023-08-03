df7ff2c2d4ac7a43036e1c0a9d25bd58f3817895
aliiiimaher <alimohammadmaher@gmail.com>
2023-08-03 23:41:59 +0330

    middle of courses-layout

--- src/pages/usersPanel/MyCoursesPage.tsx
+++ src/pages/usersPanel/MyCoursesPage.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
+import PanelSideBarMenu from "../../components/PanelSideBarMenu";
+import CourseData from "../../interfaces/CourseData";
+
 function MyCoursesPage() {
-  return <>this is all of my courses</>;
+  const [userCourses, _setUserCourses] = useState<CourseData[]>([]);
+
+  //  here we need some api for get the courses
+
+  return (
+    <>
+      <div className="panel-myCoursePage-container">
+        <div className="panel-myCoursePage-right-hand">
+          <PanelSideBarMenu />
+        </div>
+        <div className="panel-myCoursePage-left-hand">
+          {userCourses === null ? (
+            <p>Loading...</p>
+          ) : userCourses.length === 0 ? (
+            <p>No courses found</p>
+          ) : (
+            <p>{userCourses[0].title}</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MyCoursesPage;
