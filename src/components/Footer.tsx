e292bc7059ba277a6f484eb04b8123decff53e12
aliiiimaher <alimohammadmaher@gmail.com>
2023-08-06 15:19:13 +0330

    footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,124 +9,87 @@ import telegramSvg from "../assets/Pic/telegram.svg";
 function Footer() {
   return (
     <>
-      <>
-        <nav
-          className="navbar navbar-expand-lg"
-          style={{ backgroundColor: "#3a365e" }}
+      <nav className="navbar navbar-expand-lg">
+        <div
+          className="container-fluid"
+          style={{ marginLeft: "40px", marginRight: "40px" }}
         >
           <div
-            className="container-fluid"
-            style={{ marginLeft: "40px", marginRight: "40px" }}
+            className="collapse navbar-collapse menu-items"
+            id="navbarSupportedContent"
           >
-            <div
-              className="collapse navbar-collapse menu-items"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav mb-2 mb-lg-0 footer-col-3">
-                <li className="nav-item">
-                  <a className="nav-link subtitle" aria-current="page" href="#">
-                    شهر‌های زبان لرن
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    شیراز
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    تهران
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    اصفهان
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    مشهد
-                  </a>
-                </li>
-              </ul>
-              <ul className="navbar-nav mb-2 mb-lg-0 footer-col-3">
-                <li className="nav-item">
-                  <a className="nav-link subtitle" aria-current="page" href="#">
-                    درباره ما
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    مشاوره تلفنی
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    سوالات متداول
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    قوانین و مقررات
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    تماس با ما
-                  </a>
-                </li>
-              </ul>
-              <ul className="navbar-nav mb-2 mb-lg-0 footer-col-3">
-                <li className="nav-item">
-                  <a className="nav-link subtitle" aria-current="page" href="#">
-                    تلفن‌های تماس
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    ۰۹۱۶-۴۵۱-۶۴۳۲
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    ۰۹۱۰-۶۶۶-۳۵۳۰
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link subtitle" href="#">
-                    پشتیبانی
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    support@zabanlearn.com
-                  </a>
-                </li>
-              </ul>
-              <div className="follow-us-in-social">
-                <h5>ما را در شبکه‌های اجتماعی دنبال کنيد!</h5>
-                <div className="follow-us-in-social-logos">
-                  <a className="navbar-brand logo" href="#">
-                    <img src={telegramSvg} alt="Logo" width="45" height="45" />
-                  </a>
-                  <a className="navbar-brand logo" href="#">
-                    <img src={youtbubeSvg} alt="Logo" width="45" height="30" />
-                  </a>
-                  <a className="navbar-brand logo" href="#">
-                    <img src={instagramSvg} alt="Logo" width="45" height="45" />
-                  </a>
-                </div>
-              </div>
-              <div className="security-logo">
-                <a className="navbar-brand" href="#">
-                  <img src={sabtSvg} alt="Logo" width="80" height="63" />
-                  <img src={eNamadSvg} alt="Logo" width="80" height="63" />
+            <ul className="navbar-nav mb-2 mb-lg-0 footer-col-3">
+              <li className="nav-item">
+                <strong>شهر‌های زبان لرن</strong>
+              </li>
+              <li className="">شیراز</li>
+              <li className="nav-item">تهران</li>
+              <li className="nav-item">اصفهان</li>
+              <li className="nav-item">مشهد</li>
+            </ul>
+            <ul className="navbar-nav mb-2 mb-lg-0 footer-col-3">
+              <li className="nav-item">
+                <strong>درباره ما</strong>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  مشاوره تلفنی
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  سوالات متداول
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  قوانین و مقررات
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/us">
+                  تماس با ما
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav mb-2 mb-lg-0 footer-col-3">
+              <li className="nav-item">
+                <strong>تلفن‌های تماس</strong>
+              </li>
+              <li className="nav-item">۰۹۱۶-۴۵۱-۶۴۳۲</li>
+              <li className="nav-item">۰۹۱۰-۶۶۶-۳۵۳۰</li>
+              <li className="nav-item">
+                <strong>پشتیبانی</strong>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="mailto:support@zabanlearn.com">
+                  support@zabanlearn.com
+                </a>
+              </li>
+            </ul>
+            <div className="follow-us-in-social">
+              <h5>ما را در شبکه‌های اجتماعی دنبال کنيد!</h5>
+              <div className="follow-us-in-social-logos">
+                <a className="navbar-brand logo" href="#">
+                  <img src={telegramSvg} alt="Logo" width="45" height="45" />
+                </a>
+                <a className="navbar-brand logo" href="#">
+                  <img src={youtbubeSvg} alt="Logo" width="45" height="30" />
+                </a>
+                <a className="navbar-brand logo" href="#">
+                  <img src={instagramSvg} alt="Logo" width="45" height="45" />
                 </a>
               </div>
             </div>
+            <div className="security-logo">
+              <a className="navbar-brand" href="#">
+                <img src={sabtSvg} alt="Logo" width="80" height="63" />
+                <img src={eNamadSvg} alt="Logo" width="80" height="63" />
+              </a>
+            </div>
           </div>
-        </nav>
-      </>
+        </div>
+      </nav>
     </>
   );
 }
