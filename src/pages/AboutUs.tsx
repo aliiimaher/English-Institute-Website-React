acbd57deffa3a72b35cd08da447cd2864c5efc58
aliiiimaher <alimohammadmaher@gmail.com>
2023-08-18 09:58:06 +0330

    about us page updated

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,5 +1,9 @@
 import "../styles/pages/AboutUs.scss";
 
+import usofImg from "../assets/Pic/Personel/usof.png";
+import aliImg from "../assets/Pic/Personel/1675021540592.jfif";
+import msnImg from "../assets/Pic/Personel/81595622.jfif";
+
 function AboutUs() {
   return (
     <>
@@ -8,27 +12,30 @@ function AboutUs() {
           <tr>
             <th>UI/UX</th>
             <td>
+              <img src={usofImg} alt="" />
               <i>Yousof Asadi</i>
             </td>
-            <td>
+            <td className="personal-github">
               <a href="https://github.com/YousofAsadi">GitHub</a>
             </td>
           </tr>
           <tr>
             <th>Front-End</th>
             <td>
+              <img src={aliImg} alt="" />
               <i>Ali Maher</i>
             </td>
-            <td>
+            <td className="personal-github">
               <a href="https://github.com/aliiimaher">GitHub</a>
             </td>
           </tr>
           <tr>
             <th>Back-End</th>
             <td>
+              <img src={msnImg} alt="" />
               <i>Mohsen Zahmatkesh</i>
             </td>
-            <td>
+            <td className="personal-github">
               <a href="https://github.com/msnzmt">GitHub</a>
             </td>
           </tr>
