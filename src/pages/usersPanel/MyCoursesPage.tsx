a7f62c34f30b65470f0142b97f79c13a6a2f9e40
aliiiimaher <alimohammadmaher@gmail.com>
2023-08-21 12:44:53 +0330

    style bug fixed

--- src/pages/usersPanel/MyCoursesPage.tsx
+++ src/pages/usersPanel/MyCoursesPage.tsx
@@ -42,7 +42,7 @@ function MyCoursesPage() {
               هیچ دوره‌ای پیدا نشد :(
             </div>
           ) : (
-            <div>
+            <div className="panel-myCoursePage-left-hand-cardh-render">
               {userCourses.map((course, index) => (
                 <CardH
                   key={index}
